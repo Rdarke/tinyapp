d38ff30e0156bef9204442c0049a9c0e9e5462dd
Rdarke <rickydarke1@gmail.com>
2021-08-30 15:57:55 -0700

    updated URl param & refactor the GET urls by short url logic.

--- express_server.js
+++ express_server.js
@@ -26,6 +26,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const { shortURL } = req.params;
+  const longURL = urlDatabase[shortURL]
+  
+  const templateVars = { shortURL, longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
