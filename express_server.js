3fd83bf357572699bc1e8b99bbfa7886f0850715
Rdarke <rickydarke1@gmail.com>
2021-08-31 12:06:35 -0700

    Update express_server - refactor redirects

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const morgan = require('morgan');
-const app = express();
-const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -26,8 +26,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// URL's route for rendering with EJS. 
-//{ urls: urlDatabase } in object format to follow EJS template format.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -45,25 +43,25 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  
-  urlDatabase[shortURL] = {
-     longURL: req.body.longURL
-  };
-  res.redirect(`/urls/${shortURL}`);
-});
-
 app.get("/u/:shortURL", (req, res) => {
   const { shortURL } = req.params;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+
+  urlDatabase[shortURL] = longURL;
+
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
