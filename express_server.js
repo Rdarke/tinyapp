640e74dcb73d11dad4e55edf1ffc5b034defa07c
Rdarke <rickydarke1@gmail.com>
2021-08-31 17:03:20 -0700

    Update cookies requirements on express_server

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 function generateRandomString() {
   const length = 6
@@ -27,7 +29,11 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  
   res.render("urls_index", templateVars);
 });
 
@@ -38,7 +44,8 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const { shortURL } = req.params;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL, longURL };
+  const username = req.cookies["username"];
+  const templateVars = { shortURL, longURL, username };
 
   res.render("urls_show", templateVars);
 });
@@ -66,7 +73,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const { shortURL } = req.params;
 
   delete urlDatabase[shortURL];
-
   res.redirect("/urls");
 });
 
@@ -81,7 +87,13 @@ app.post("/urls/:shortURL", (req, res) => {
   const longURL = req.body.longURL;
 
   urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls`);
+});
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+
+  res.cookie("username", username);
   res.redirect(`/urls`);
 });
 
