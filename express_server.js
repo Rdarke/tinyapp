9d0451954bb75f6be19425237836c986de096498
Rdarke <rickydarke1@gmail.com>
2021-08-31 21:05:53 -0700

    Implement /logout endpoint too clear the username cookie

--- express_server.js
+++ express_server.js
@@ -98,6 +98,12 @@ app.post("/login", (req, res) => {
   res.redirect(`/urls`);
 });
 
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+
+  res.clearCookie("username", username);
+  res.redirect(`/urls`);
+});
 
 
 app.listen(PORT, () => {
