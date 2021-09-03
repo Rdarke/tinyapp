e2d3e430aae6911d5a7efe322f8db710e0185d2a
Rdarke <rickydarke1@gmail.com>
2021-09-02 17:05:12 -0700

    Add BcryptJS package

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
 
