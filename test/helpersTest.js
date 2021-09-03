619ca3fec89a674264596f279ed602f8999547e3
Rdarke <rickydarke1@gmail.com>
2021-09-02 22:50:20 -0700

    Write testing via mocha & chai

---
+++ test/helpersTest.js
@@ -0,0 +1,25 @@
+const { assert, expect } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a userID Key using via email call back', function() {
+    
+    const output = findUserByEmail(testUsers, (key) => key.email === "user@example.com")
+    const expectedOutput = "userRandomID";
+    assert.strictEqual(output, expectedOutput);
+  });
+});
